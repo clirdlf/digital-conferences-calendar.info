fd29db3a06384ff0302199bacacb7cd195d46da5
Wayne Graham <wgraham@clir.org>
2016-02-02 16:58:51 -0500

    Install API key

--- js/main.js
+++ js/main.js
@@ -5,11 +5,12 @@ $(document).ready(function() {
     // THIS KEY WON'T WORK IN PRODUCTION!!!
     // To make your own Google API key, follow the directions here:
     // http://fullcalendar.io/docs/google_calendar/
-    googleCalendarApiKey: 'AIzaSyDcnW6WejpTOCffshGDDb4neIrXVUA1EAE',
-    // googleCalendarApiKey: 'g2hval0pee3rmrv4f3n9hp9cok',
+    //googleCalendarApiKey: 'AIzaSyDcnW6WejpTOCffshGDDb4neIrXVUA1EAE',
+    googleCalendarApiKey: 'AIzaSyA5W2MDJs9uknQv6Cn4OZ07K-wtvkoqYwE',
 
     // US Holidays
     // events: 'usa__en@holiday.calendar.google.com',
+    events: 'g2hval0pee3rmrv4f3n9hp9cok@group.calendar.google.com',
 
     eventClick: function(event) {
       // opens events in a popup window
