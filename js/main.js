80b03191596ac5ca2752f11d791f952ea4d3bcf2
Wayne Graham <wgraham@clir.org>
2018-07-11 12:45:58 -0400

    Generic updates

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
 
-  $('#calendar').fullCalendar({
+    $('#calendar').fullCalendar({
 
-    // THIS KEY WON'T WORK IN PRODUCTION!!!
-    // To make your own Google API key, follow the directions here:
-    // http://fullcalendar.io/docs/google_calendar/
-    //googleCalendarApiKey: 'AIzaSyDcnW6WejpTOCffshGDDb4neIrXVUA1EAE',
-    googleCalendarApiKey: 'AIzaSyA5W2MDJs9uknQv6Cn4OZ07K-wtvkoqYwE',
+        // THIS KEY WON'T WORK IN PRODUCTION!!!
+        // To make your own Google API key, follow the directions here:
+        // http://fullcalendar.io/docs/google_calendar/
+        //googleCalendarApiKey: 'AIzaSyDcnW6WejpTOCffshGDDb4neIrXVUA1EAE',
+        googleCalendarApiKey: 'AIzaSyA5W2MDJs9uknQv6Cn4OZ07K-wtvkoqYwE',
 
-    // US Holidays
-    // events: 'usa__en@holiday.calendar.google.com',
-    events: 'g2hval0pee3rmrv4f3n9hp9cok@group.calendar.google.com',
+        // US Holidays
+        // events: 'usa__en@holiday.calendar.google.com',
+        events: 'g2hval0pee3rmrv4f3n9hp9cok@group.calendar.google.com',
 
-    eventClick: function(event) {
-      // opens events in a popup window
-      window.open(event.url, 'gcalevent', 'width=700,height=600');
-      return false;
-    },
+        eventClick: function(event) {
+            // opens events in a popup window
+            window.open(event.url, 'gcalevent', 'width=700,height=600');
+            return false;
+        },
 
-    loading: function(bool) {
-      $('#loading').toggle(bool);
-    }
+        loading: function(bool) {
+            $('#loading').toggle(bool);
+        }
 
-  });
+    });
 
 });
