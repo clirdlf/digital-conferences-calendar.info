e569469e0d00e9088be06a6360ceab8378d4a5c2
Wayne Graham <wgraham@clir.org>
2018-07-11 15:44:39 -0400

    Update to FullCalendar 3
    Updates JS calls to FullCalendar 3.9.0
    Adds DLF Zoom Calendar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
+    var initialLocaleCode = 'en';
 
     $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month, listDay, listWeek,'
+        },
+
+        themeSystem: 'bootstrap4',
+
+        // locale: initialLocaleCode, // Internationalization
+
+        // displayEventTime: false, // don't show the time column in list view
+        // customize the button names,
+          // otherwise they'd all just say "list"
+          views: {
+            listDay: { buttonText: 'list day' },
+            listWeek: { buttonText: 'list week' }
+          },
+          // navLinks: true,
 
         // THIS KEY WON'T WORK IN PRODUCTION!!!
         // To make your own Google API key, follow the directions here:
@@ -10,7 +29,28 @@ $(document).ready(function() {
 
         // US Holidays
         // events: 'usa__en@holiday.calendar.google.com',
-        events: 'g2hval0pee3rmrv4f3n9hp9cok@group.calendar.google.com',
+        // events: {
+        //     googleCalendarId: 'g2hval0pee3rmrv4f3n9hp9cok@group.calendar.google.com',
+        //     className: 'community-calendar',
+        // },
+        // c9n96oka3aaha4499lnq4jma4o%40group.calendar.google.com
+        eventSources: [
+            {
+                googleCalendarId: 'g2hval0pee3rmrv4f3n9hp9cok@group.calendar.google.com',
+                className: 'dlf-community-events',
+                // color: 'blue'
+            },
+            // {
+            //     googleCalendarId: 'c9n96oka3aaha4499lnq4jma4o%40group.calendar.google.com',
+            //     className: 'dlf-zoom',
+            //     color: 'orange',
+            // },
+            {
+                googleCalendarId: '1nlqihbdhsca7r7npe93so66kk@group.calendar.google.com',
+                className: 'dlf-zoom',
+                color: 'orange',
+            }
+        ],
 
         eventClick: function(event) {
             // opens events in a popup window
@@ -23,5 +63,23 @@ $(document).ready(function() {
         }
 
     });
+
+
+    // build the locale selector's options
+    // $.each($.fullCalendar.locales, function(localeCode) {
+    //   $('#locale-selector').append(
+    //     $('<option/>')
+    //       .attr('value', localeCode)
+    //       .prop('selected', localeCode == initialLocaleCode)
+    //       .text(localeCode)
+    //   );
+    // });
+    //
+    // // when the selected option changes, dynamically change the calendar option
+    // $('#locale-selector').on('change', function() {
+    //   if (this.value) {
+    //     $('#calendar').fullCalendar('option', 'locale', this.value);
+    //   }
+    // });
 
 });
