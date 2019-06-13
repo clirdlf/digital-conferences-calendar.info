53534e270a25db035e0ec7b50b318a93e995217f
Wayne Graham <wgraham@clir.org>
2019-06-13 09:35:45 -0400

    Update for @fullcalendarv4

---
+++ Gulpfile.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const del = require('del');
+const gulp = require("gulp");
+const merge = require("merge-stream");
+
+const pkg = require('./package.json');
+
+function clean() {
+    return del(['./vendor/']);
+}
+
+function modules() {
+    var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
+        .pipe(gulp.dest('./vendor/bootstrap'));
+    var jquery = gulp.src([
+            './node_modules/jquery/dist/*',
+        ])
+        .pipe(gulp.dest('./vendor/jquery'));
+    var fullcalendar_core = gulp.src(
+        './node_modules/@fullcalendar/core/**/*',
+    ).pipe(gulp.dest('./vendor/fullcalendar/core'));
+    var fullcalendar_daydrid = gulp.src(
+        './node_modules/@fullcalendar/daygrid/**/*',
+    ).pipe(gulp.dest('./vendor/fullcalendar/daygrid'));
+    var fullcalendar_gcal = gulp.src(
+        './node_modules/@fullcalendar/google-calendar/**/*',
+    ).pipe(gulp.dest('./vendor/fullcalendar/google-calendar'));
+    var fullcalendar_list = gulp.src(
+        './node_modules/@fullcalendar/list/**/*',
+    ).pipe(gulp.dest('./vendor/fullcalendar/list'));
+    var fullcalendar_list = gulp.src(
+        './node_modules/@fullcalendar/bootstrap/**/*',
+    ).pipe(gulp.dest('./vendor/fullcalendar/bootstrap'));
+
+    return merge(bootstrap, jquery, fullcalendar_core, fullcalendar_daydrid, fullcalendar_gcal);
+}
+
+const vendor = gulp.series(clean, modules);
+
+exports.vendor = vendor;
+exports.clean = clean;
+exports.default = vendor;
--- js/main.js
+++ js/main.js
@@ -1,51 +1,37 @@
-(function(){
-  'use strict';
+(function() {
+    'use strict';
 
-  var z_account = "E17D945D-B632-4031-BDA8-ED8F260A04F0";
-  var z_collector = "clir.informz.net";
-  var z_cookieDomain = ".digital-conferences-calendar.info";
+    var z_account = "E17D945D-B632-4031-BDA8-ED8F260A04F0";
+    var z_collector = "clir.informz.net";
+    var z_cookieDomain = ".digital-conferences-calendar.info";
 
-  (function (e, o, n, r, t, a, s) { e[t] || (e.GlobalSnowplowNamespace = e.GlobalSnowplowNamespace || [], e.GlobalSnowplowNamespace.push(t), e[t] = function () { (e[t].q = e[t].q || []).push(arguments) }, e[t].q = e[t].q || [], a = o.createElement(n), s = o.getElementsByTagName(n)[0], a.async = 1, a.src = r, s.parentNode.insertBefore(a, s)) }(window, document, "script", "https://"+z_collector+"/web_trk/sp.js", "informz_trk")), informz_trk("newTracker", "infz", z_collector + "/web_trk/collector/", { appId: z_account, cookieDomain: z_cookieDomain }), informz_trk("setUserIdFromLocation", "_zs"), informz_trk("enableActivityTracking", 30, 15); informz_trk("trackPageView", null);
+    (function(e, o, n, r, t, a, s) {
+        e[t] || (e.GlobalSnowplowNamespace = e.GlobalSnowplowNamespace || [], e.GlobalSnowplowNamespace.push(t), e[t] = function() {
+            (e[t].q = e[t].q || []).push(arguments)
+        }, e[t].q = e[t].q || [], a = o.createElement(n), s = o.getElementsByTagName(n)[0], a.async = 1, a.src = r, s.parentNode.insertBefore(a, s))
+    }(window, document, "script", "https://" + z_collector + "/web_trk/sp.js", "informz_trk")), informz_trk("newTracker", "infz", z_collector + "/web_trk/collector/", {
+        appId: z_account,
+        cookieDomain: z_cookieDomain
+    }), informz_trk("setUserIdFromLocation", "_zs"), informz_trk("enableActivityTracking", 30, 15);
+    informz_trk("trackPageView", null);
 })();
 
-$(document).ready(function() {
-    var initialLocaleCode = 'en';
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
 
-    $('#calendar').fullCalendar({
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: ['dayGrid', 'googleCalendar', 'list', 'bootstrap'],
+        googleCalendarApiKey: 'AIzaSyA5W2MDJs9uknQv6Cn4OZ07K-wtvkoqYwE',
         header: {
             left: 'prev,next today',
             center: 'title',
-            right: 'month, listDay, listWeek,'
+            right: 'dayGridMonth, listDay, listWeek,'
         },
-
-        themeSystem: 'bootstrap4',
-
-        // locale: initialLocaleCode, // Internationalization
-
-        // displayEventTime: false, // don't show the time column in list view
-        // customize the button names,
-        // otherwise they'd all just say "list"
-        views: {
-            listDay: {
-                buttonText: 'list day'
-            },
-            listWeek: {
-                buttonText: 'list week'
-            }
-        },
-        // navLinks: true,
-
-        // THIS KEY WON'T WORK IN PRODUCTION!!!
-        // To make your own Google API key, follow the directions here:
-        // http://fullcalendar.io/docs/google_calendar/
-        //googleCalendarApiKey: 'AIzaSyDcnW6WejpTOCffshGDDb4neIrXVUA1EAE',
-        googleCalendarApiKey: 'AIzaSyA5W2MDJs9uknQv6Cn4OZ07K-wtvkoqYwE',
-
-        // US Holidays
-        // events: 'usa__en@holiday.calendar.google.com',
+        themeSystem: 'bootstrap',
         eventSources: [{
                 googleCalendarId: 'g2hval0pee3rmrv4f3n9hp9cok@group.calendar.google.com',
                 className: 'dlf-community-events',
+                color: '#3a87ad'
                 // color: 'blue'
             },
             {
@@ -54,35 +40,87 @@ $(document).ready(function() {
                 color: '#EF6C00',
             }
         ],
-
-        eventClick: function(event) {
-            // opens events in a popup window
-            window.open(event.url, 'gcalevent', 'width=700,height=600');
-            return false;
-        },
-
-        loading: function(bool) {
-            $('#loading').toggle(bool);
+        views: {
+            listDay: {
+                buttonText: 'list day'
+            },
+            listWeek: {
+                buttonText: 'list week'
+            }
         }
+
+        // eventClick: function(event) {
+        //     // opens events in a popup window
+        //     window.open(event.url, 'gcalevent', 'width=700,height=600');
+        //     return false;
+        // }
 
     });
 
+    calendar.render();
 
-    // build the locale selector's options
-    // $.each($.fullCalendar.locales, function(localeCode) {
-    //   $('#locale-selector').append(
-    //     $('<option/>')
-    //       .attr('value', localeCode)
-    //       .prop('selected', localeCode == initialLocaleCode)
-    //       .text(localeCode)
-    //   );
-    // });
+    console.log(calendarEl);
+});
+
+$(document).ready(function() {
+
+
+    // var initialLocaleCode = 'en';
     //
-    // // when the selected option changes, dynamically change the calendar option
-    // $('#locale-selector').on('change', function() {
-    //   if (this.value) {
-    //     $('#calendar').fullCalendar('option', 'locale', this.value);
-    //   }
+    // $('#calendar').fullCalendar({
+    //     header: {
+    //         left: 'prev,next today',
+    //         center: 'title',
+    //         right: 'month, listDay, listWeek,'
+    //     },
+    //
+    //     themeSystem: 'bootstrap4',
+    //
+    //     // locale: initialLocaleCode, // Internationalization
+    //
+    //     // displayEventTime: false, // don't show the time column in list view
+    //     // customize the button names,
+    //     // otherwise they'd all just say "list"
+    //     views: {
+    //         listDay: {
+    //             buttonText: 'list day'
+    //         },
+    //         listWeek: {
+    //             buttonText: 'list week'
+    //         }
+    //     },
+    //     // navLinks: true,
+    //
+    //     // THIS KEY WON'T WORK IN PRODUCTION!!!
+    //     // To make your own Google API key, follow the directions here:
+    //     // http://fullcalendar.io/docs/google_calendar/
+    //     //googleCalendarApiKey: 'AIzaSyDcnW6WejpTOCffshGDDb4neIrXVUA1EAE',
+    //     googleCalendarApiKey: 'AIzaSyA5W2MDJs9uknQv6Cn4OZ07K-wtvkoqYwE',
+    //
+    //     // US Holidays
+    //     // events: 'usa__en@holiday.calendar.google.com',
+    //     eventSources: [{
+    //             googleCalendarId: 'g2hval0pee3rmrv4f3n9hp9cok@group.calendar.google.com',
+    //             className: 'dlf-community-events',
+    //             // color: 'blue'
+    //         },
+    //         {
+    //             googleCalendarId: '1nlqihbdhsca7r7npe93so66kk@group.calendar.google.com',
+    //             className: 'dlf-zoom',
+    //             color: '#EF6C00',
+    //         }
+    //     ],
+    //
+    //     eventClick: function(event) {
+    //         // opens events in a popup window
+    //         window.open(event.url, 'gcalevent', 'width=700,height=600');
+    //         return false;
+    //     },
+    //
+    //     loading: function(bool) {
+    //         $('#loading').toggle(bool);
+    //     }
+    //
     // });
 
 });
